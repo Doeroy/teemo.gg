2aba3765519dbd222a9864a36f410307f1f6775b
Daniel Asiamah <danasiamah003@gmail.com>
2025-03-13 22:27:32 -0700

    base database setup

---
+++ backend/LEAGUE_TESTER/models.py
@@ -0,0 +1,22 @@
+# models.py
+from extend import db  # Import db from extensions
+
+class SummonerProfile(db.Model):
+    __tablename__ = 'summoner_prof_test1'  # Make sure this matches your table name
+
+    summonerID = db.Column(db.String(50), primary_key=True)
+    riot_id = db.Column(db.String(45), nullable=False)
+    riot_tag = db.Column(db.String(45), nullable=False)
+    puuid = db.Column(db.String(45), unique=True, nullable=False)
+    reigon = db.Column(db.String(45), nullable=False)
+
+    def to_dict(self):
+        return {
+            'summonerID': self.summonerID,
+            'riot_id': self.riot_id,
+            'riot_tag': self.riot_tag,
+            'puuid': self.puuid,
+            'reigon': self.reigon
+        }
+
+
---
+++ backend/LEAGUE_TESTER/test_summoner.py
@@ -0,0 +1,18 @@
+import requests
+import json
+
+url = "http://127.0.0.1:5000/add_summoner"
+headers = {"Content-Type": "application/json"}
+data = {
+    "summonerID": "Summoner789",
+    "riot_id": "PlayerC",
+    "riot_tag": "NA1",
+    "puuid": "abcd-1234-2est",
+    "reigon": "NA"
+}
+
+response = requests.post(url, headers=headers, json=data)
+
+# Print the status code and response text
+print(f"Status Code: {response.status_code}")
+print(f"Response: {response.text}")
